4ae8e9c87661694a8580170d69687bfb832063a2
ic <iancronizer@gmail.com>
2022-11-09 10:39:09 -0500

    updated 6.12

--- 6.12.cpp
+++ 6.12.cpp
@@ -2,7 +2,6 @@
 #include<iostream>
 #include<iomanip>
 #include <string>
-using namespace std;
 
 //I know i should have used switch, but it was right there on the edge for me
 double car(double hours)
@@ -37,26 +36,26 @@ int main()
 {
 	//get input
 	double hours, hours2, hours3;
-	cout << "Enter the number of hours Car 1: ";
-	cin >> hours;
-	cout << "Enter the number of hours Car 2: ";
-	cin >> hours2;
-	cout << "Enter the number of hours Car 3: ";
-	cin >> hours3;
-	cout << "Car     Hours      Charge" << endl;
+	std::cout << "Enter the number of hours Car 1: ";
+	std::cin >> hours;
+	std::cout << "Enter the number of hours Car 2: ";
+	std::cin >> hours2;
+	std::cout << "Enter the number of hours Car 3: ";
+	std::cin >> hours3;
+	std::cout << "Car     Hours      Charge" << std::endl;
 	//define result
 	double result = car(hours);
 
-	cout << "2" << setw(10) << hours << setw(14) << result << endl;
+	std::cout << "2" << std::setw(10) << hours << std::setw(14) << result << std::endl;
 	//since function car can only take 1 variable, we must change it here
 	hours = hours2;
 	double result2 = car(hours);
 	//cout result2
-	cout << "2" << setw(10) << hours << setw(14) << result2 << endl;
+	std::cout << "2" << std::setw(10) << hours << std::setw(14) << result2 << std::endl;
 	//since function car can only take 1 variable, we must change it here
 	hours = hours3;
 	double result3 = car(hours);
 	//cout result3
-	cout <<"3" << setw(10) << hours << setw(14) << result3;
-
+	std::cout <<"3" << std::setw(10) << hours << std::setw(14) << result3;
+    std::cout << std::endl;
 }
